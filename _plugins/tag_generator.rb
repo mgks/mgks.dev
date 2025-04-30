6424896a6d414f328651798a93628752be9bf5d6
mgks <hello@mgks.dev>
2025-04-30 11:55:00 +0530

    Update tags.html
    Added new plugin

---
+++ _plugins/tag_generator.rb
@@ -0,0 +1,25 @@
+module Jekyll
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.tags.keys.each do |tag|
+        site.pages << TagPage.new(site, site.source, tag)
+      end
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, base, tag)
+      @site = site
+      @base = base
+      @dir = File.join('tag', tag.downcase)
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['tag'] = tag
+      self.data['title'] = "#{tag.capitalize} Articles"
+    end
+  end
+end 
