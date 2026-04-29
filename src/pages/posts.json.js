9f0943bf31e58b799419364b19bfbb38567526bc
Ghazi <hello@mgks.dev>
2026-04-29 07:23:21 +0530

    Update projects

--- src/pages/posts.json.js
+++ src/pages/posts.json.js
@@ -4,7 +4,7 @@ export async function GET() {
   const posts = await getCollection('blog');
   
   const map = posts.map(post => ({
-    slug: post.slug, // This will be the NEW full slug (2026-01-27-title)
+    slug: post.id, // This will be the NEW full slug (2026-01-27-title)
     title: post.data.title
   }));
 
